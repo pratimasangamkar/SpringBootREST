fcbb6898590c848537282d45769711a53fa8b819
svilas <svilas@DIN51002889.corp.capgemini.com>
2017-09-28 12:46:20 +0530

    Updated Properties File

--- Java8WorkSpace/SpringBootDemo/src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ Java8WorkSpace/SpringBootDemo/src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -1,12 +1,35 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={JacksonAutoConfiguration.class,WebSocketAutoConfiguration.class})
+
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args ->{
+			System.out.println("Spring Bean Defn names");
+			String[] beans = ctx.getBeanDefinitionNames();
+			Arrays.sort(beans);
+			Arrays.asList(beans).forEach(System.out::println);
+			
+		};
+	}
+	
 }
